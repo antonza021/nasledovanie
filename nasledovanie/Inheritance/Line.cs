302362e226482475ae2f4844d86e0a3a9f4a7126
Kudryavtsevaa <Kudryavtsevaa@310D-6>
2023-12-15 14:22:22 +0700

    Добавьте файлы проекта.

---
+++ nasledovanie/Inheritance/Line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasledovanie.Inheritance
+{
+    internal class Line : Point
+    {
+        public override string Name()
+        {
+            return "Отрезок";
+        }
+        public double X2;
+        public double Y2;
+        public Line(double x, double y, double x2, double y2) : base(x, y)
+        {
+            X2 = x2;
+            Y2 = y2;
+        }
+        public override double Size()
+        {
+            return Math.Abs(Math.Sqrt(Math.Pow(X - X2, 2) + Math.Pow(Y - Y2, 2)));
+        }
+    }
+}
